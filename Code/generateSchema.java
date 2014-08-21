cf8263d98b7378eb22ed800c743d5ada06e73918
Sebastian Rieger <sebastian2406@t-online.de>
2014-08-21 16:24:49 +0200

    SMD angefangen und Jackson Schemaerstellung hinzugefügt
    weiter bei SMD beschreibung des Bildes........

---
+++ Code/generateSchema.java
@@ -0,0 +1,10 @@
+public String generateSchema(OPMObject opmObject) throws JsonProcessingException,JsonMappingException{
+		ObjectMapper m = new ObjectMapper();
+		SchemaFactoryWrapper visitor = new SchemaFactoryWrapper();
+
+		m.acceptJsonFormatVisitor(m.constructType(opmObject.getClass()), 
+			visitor);
+		JsonSchema jsonSchema = visitor.finalSchema();
+		return m.writerWithDefaultPrettyPrinter().
+			writeValueAsString(jsonSchema);
+}
