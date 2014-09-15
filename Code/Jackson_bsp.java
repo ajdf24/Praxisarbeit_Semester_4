a043ef694886ae58b42e3392580e5c394adbed48
Sebastian Rieger <sebastian2406@t-online.de>
2014-09-15 11:15:04 +0200

    Finaler Commit. Projektarbeit ist jetzt entgültig fertiggestellt

--- Code/Jackson_bsp.java
+++ Code/Jackson_bsp.java
@@ -1,11 +1,12 @@
 public String serialize(OPMObject opmObject) {
-	ObjectMapper mapper = new ObjectMapper();
-	AnnotationIntrospector inspector = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
-	mapper.setAnnotationIntrospector(inspector);
-
 	if (opmObject == null) {
 			throw new IllegalArgumentException("OPMObject can not be null!");
 	}
+	
+	ObjectMapper mapper = new ObjectMapper();
+	AnnotationIntrospector inspector = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
+	mapper.setAnnotationIntrospector(inspector);
+	
 	try {
 		return mapper.writeValueAsString(opmObject);
 	} catch (JsonProcessingException e) {
